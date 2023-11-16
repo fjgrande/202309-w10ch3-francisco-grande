eb710282e301c4c46df62f286de01d93939face6
Francisco Grande <fjgrandv@gmail.com>
2023-11-16 22:48:41 +0100

    Fix route in app notfound

--- src/server/app.ts
+++ src/server/app.ts
@@ -3,7 +3,6 @@ import express from "express";
 import morgan from "morgan";
 import pingRouter from "../features/ping/router/pingRouter.js";
 import { notFound } from "./middlewares/errors/errorMiddleware.js";
-import characterRouter from "../features/characters/router/characterRouter.js";
 
 const app = express();
 app.disable("x-powered-by");
@@ -13,8 +12,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/", pingRouter);
-
-app.use("/characters", characterRouter);
 
 app.use(notFound);
 
