7f0c2baa9854cce2475c187b132378ac3e9d5ca3
Francisco Grande <fjgrandv@gmail.com>
2023-11-16 22:44:48 +0100

    Add notfound in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,12 +2,20 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import pingRouter from "../features/ping/router/pingRouter.js";
+import { notFound } from "./middlewares/errors/errorMiddleware.js";
+import characterRouter from "../features/characters/router/characterRouter.js";
 
 const app = express();
+app.disable("x-powered-by");
+
 app.use(express.json());
 
 app.use(morgan("dev"));
 
 app.use("/", pingRouter);
+
+app.use("/characters", characterRouter);
+
+app.use(notFound);
 
 export default app;
